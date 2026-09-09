10f9a3f8c01bd4c81924d410c3466cfc345a20a5
Kat-Long <long1029@gmail.com>
2026-09-09 19:49:31 +0700

    リポジトリをPage配下のみに整理（他フォルダを追跡除外・一時ファイル無視）

--- Mail/check_progress.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-for i in range(30):
-    time.sleep(10)
-    sz = os.path.getsize('extract_v3_final.txt')
-    print(f'Check {i+1}: Size = {sz} bytes')
-    if sz > 100000:
-        print('File is growing, script is running...')
--- Mail/wait_and_check.py
+++
@@ -1,8 +0,0 @@
-import time
-import os
-
-print("Waiting for script to finish...")
-time.sleep(45)
-
-sz = os.path.getsize('extract_v3_profiles.txt')
-print(f'File size: {sz} bytes')
